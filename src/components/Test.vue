f8205e777a2ee05bbd55bdbdf00587d332c8f5d7
taozhi <taozhi1028@gmail.com>
2022-04-16 11:26:25 +0800

    add demo

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ id }}</div>
+</template>
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    id: string | null;
+  }>(),
+  { id: null }
+);
+</script>
